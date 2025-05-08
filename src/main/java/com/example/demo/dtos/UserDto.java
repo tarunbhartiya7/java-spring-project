1ddbba459b3a3bb29504553fa962563cb8c81e71
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2025-05-09 00:18:17 +0530

    json decorators

--- src/main/java/com/example/demo/dtos/UserDto.java
+++ src/main/java/com/example/demo/dtos/UserDto.java
@@ -1,11 +1,21 @@
 package com.example.demo.dtos;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+// @JsonIgnore to ignore the field during serialization
+// @JsonProperty("user_id") to rename the field during serialization
+// @JsonFormat(pattern = 'yyyy-MM-dd HH:mm:ss') to format the date
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonProperty("user_id")
     private Long id;
     private String name;
     private String email;
