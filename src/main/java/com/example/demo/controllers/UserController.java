c3d3bfa768356ff839858be3be33d3ce1003ba0e
Tarun Bhartiya <tarunbhartiya7@gmail.com>
2025-05-09 11:08:33 +0530

    add request header

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +27,9 @@ public class UserController {
     // @RequestMapping("/users")
     @GetMapping
     public Iterable<UserDto> getAllUsers(
+            @RequestHeader(value = "X-Auth-Token", required = false) String authToken,
             @RequestParam(required = false, defaultValue = "id") String sort) {
+        System.out.println("Auth Token: " + authToken);
         if (!Set.of("id", "name", "email").contains(sort)) {
             throw new IllegalArgumentException("Invalid sort parameter");
         }
